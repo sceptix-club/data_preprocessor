3bcf50f4d70397d3e349b2d900ba7dc67cc6774b
Ashish Y G <ashishyg9@gmail.com>
2024-10-02 20:55:23 +0530

    computed the output in the form of textfile

--- test.py
+++ test.py
@@ -1,4 +1,18 @@
 import pdfplumber
-with pdfplumber.open(r'data_preprocessor\data\acetone-acs-l.pdf') as pdf:
-    data =pdf.pages[0].extract_text()
-    print(data)
+
+with pdfplumber.open(r'data\acetone-acs-l.pdf') as pdf:
+    
+    text1 = []
+    
+    for page in pdf.pages:
+       
+        text = page.extract_text()
+        if text: 
+            text1.append(text)
+
+text2 = "\n".join(text1)
+
+print(text2)
+
+with open("extracted_text.txt", "w") as text_file:
+    text_file.write(text2)
