1b47da9feb802b637618b2b32fbd814932a54a7e
Del-w <23a59.delwin@sjec.ac.in>
2024-10-04 16:30:08 +0530

    Extracting text characteristics

--- prepro/text_char.py
+++ prepro/text_char.py
@@ -1,4 +1,6 @@
 import pymupdf
+import json
+import tabula as tb
 
 doc = pymupdf.open("data/acetone-acs-l.pdf")
 page = doc[0]
@@ -21,13 +23,12 @@ def flags_decomposer(flags):
         l.append("bold")
     return ", ".join(l)
 
-def print_text(doc):
-    text = chr(12).join([page.get_text() for page in doc])
-
-# read page text as a dictionary, suppressing extra spaces in CJK fonts
 blocks = page.get_text("dict", flags=11)["blocks"]
 for b in blocks:  # iterate through the text blocks
     for l in b["lines"]:  # iterate through the text lines
         for s in l["spans"]:  # iterate through the text spans
-            print(s["origin"], s["text"])
+            print(s["origin"], s["text"],s["size"])
+            
+            
+            
            
